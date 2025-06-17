b31ca01e305eb6fa74a1123d0caeb422b43be33c
hisravya <168511504+hisravya@users.noreply.github.com>
2025-06-17 10:23:00 +0530

    Add files via upload

---
+++ packagedemo.java
@@ -0,0 +1,14 @@
+import arithmetic.arithmeticoperations;
+class packagedemo
+{
+public static void main(String[]args)
+{
+arithmeticoperations art=new arithmeticoperations();
+System.out.println("sum="+art.add(2,4));
+System.out.println("sub="+art.sub(4,2));
+System.out.println("div="+art.div(4,2));
+System.out.println("mul="+art.mul(2,4));
+System.out.println("modula="+art.mod(5,2));
+}
+}
+ 
