54d5339c3f1019e4817ebeec763402550e971c49
hisravya <168511504+hisravya@users.noreply.github.com>
2025-06-17 10:35:24 +0530

    Add files via upload

---
+++ TrainService.java
@@ -0,0 +1,16 @@
+package com.aits.Traveldemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("trainservice")
+public class TrainService implements TransportService {
+
+	@Override
+	public void bookTicket(String source, String destination) {
+		System.out.println("Ticket has been booked through trainservice from"+source+"to"+destination);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TransportService.java
@@ -0,0 +1,8 @@
+package com.aits.Traveldemo;
+
+public interface TransportService {
+	
+	void bookTicket(String source, String destination);
+	
+
+}
