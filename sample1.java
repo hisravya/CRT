71cf2931ee2a4fe0ccf0ebb9100d760e563714aa
hisravya <168511504+hisravya@users.noreply.github.com>
2025-06-17 10:19:56 +0530

    Add files via upload

--- sample1.java
+++ sample1.java
@@ -1,10 +1,7 @@
-import java.io.File;
-FileWriter writer = new FileWriter("output.txt");
-Writer.write("hello,java FileWriter!");
-Writer.close();
-FileReader reader = new FileReader("output.txt");
-int ch;
-while((ch=reader.read())!=-1){
-    System.out.print((char)ch);
+import java.io.file;
+File myFile =newFile("example.txt");
+    if(myFile.exists()){
+    System.out.println("File exists:"myFile.getName());
+}else{
+    System.out.println("File does not exist");
 }
-reader.close();
