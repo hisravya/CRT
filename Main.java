3f8b24dbd8288c4c4b3c12807e1f32a49ac5d4b0
hisravya <168511504+hisravya@users.noreply.github.com>
2025-06-17 10:22:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+ 
+public class Main{
+    public static void main(String[] args){
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        //max threads that i can have is 3
+        //task
+        Runnable task = ()->{
+            System.out.println(Thread.currentThread().getName()+"is running");
+            try{
+                Thread.sleep(2000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        };
+        //we have 3 threads but ill execute 5 process/task
+        for(int i=0;i<5;i++){
+            pool.submit(task);
+        }
+ 
+        //first 3 threads will execute 3 tasks in 2 sec
+        //last 2 tasks will get executed by 2 threds for additional 2 sec
+        //total time taken is 4 sec
+        pool.shutdown();
+    }
+ 
+}
